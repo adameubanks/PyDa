62248a56d7c9a62e161b4f2cd4eb9a4a6a0537bb
KhanradCoder <akhanrade@gmail.com>
2015-02-26 09:51:37 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#these are all the dependencies needed to successfully install PyDa
+#MAKE SURE PYTHON 2.7 AND PYAUDIO ARE INSTALLED!
+
+
+#speech
+sudo apt-get install espeak python-espeak 
+
+#speech recognition
+sudo apt-get install pip
+sudo pip install SpeechRecognition
+
+#wx python
+sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-doc wx2.8-examples wx2.8-headers wx2.8-i18n
+
+#webbrowser
+sudo apt-get install webbrowser
+
+#wikipedia
+sudo pip install wikipedia
+
+#translate
+sudo pip install goslate
+
+#refresh
+sudo apt-get upgrade
+sudo apt-get update
